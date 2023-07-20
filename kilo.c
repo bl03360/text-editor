f3183a9e8c12844412e21a8672258f0e350c2b28
bl03360 <bkl050398@gmail.com>
2023-07-19 20:11:58 -0400

    Makefile for ease of creating executable Implementing text editor enabling/disabling of raw mode for terminal, Implementing buffer for string, Disabling software flow controls

---
+++ kilo.c
@@ -0,0 +1,188 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define ABUF_INIT {NULL, 0}
+
+struct editorConfig {
+	int screencols;
+	int screenrows;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
+
+struct abuf {
+	char *b;
+	int len;
+};
+
+void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2j", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	perror(s);
+	exit(1);
+}
+
+void disableRawMode() {
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
+		die("tcsetattr"); 
+	}
+}
+
+void enableRawMode() {
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
+		die("tcgetattr");
+	}
+
+	atexit(disableRawMode);
+
+	struct termios raw = E.orig_termios;
+	
+	raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag &= ~(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die("tcsetattr");
+	}
+}
+
+char editorReadKey() {
+	int nread;
+	char c;
+	while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if(nread == -1 && errno != EAGAIN) {
+			die("read");
+		}
+	}
+	return c;
+}
+
+int getCursorPosition(int *cols, int *rows) {
+	char buf[32];
+	unsigned int i = 0;
+
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+		return -1;
+	}
+
+	printf("\r\n");
+	
+	while(i < sizeof(buf) - 1) {
+		if(read(STDIN_FILENO, &buf[i], 1) != 1) {
+			break;
+		}
+		if(buf[i] == 'R') {
+			break;
+		}
+		
+		i++;
+	}
+
+	buf[i] = '\0';
+	
+	if(buf[0] != '\x1b' || buf[1] != '[') {
+		return -1;
+	}
+	if(sscanf(&buf[2], "%d;%d", cols, rows) != 2) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int getWindowSize(int *cols, int *rows) {
+	struct winsize ws;
+
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) {
+			return -1;
+		}
+		return getCursorPosition(cols, rows);
+	}
+	else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+	char *new = realloc(ab->b, ab->len + len);
+	if (new == NULL) {
+		return;
+	}
+
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+	free(ab->b);
+}
+
+void editorProcessKeyPress() {
+	char c = editorReadKey();
+	switch(c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2j", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+			break;
+	}
+}
+
+void initEditor() {
+	if(getWindowSize(&E.screencols, &E.screenrows) == -1) {
+		die("getWindowSize");
+	}
+}
+
+void editorDrawRows(struct abuf *ab) {
+	int y;
+	for(y = 0; y < E.screenrows; y++) {
+		abAppend(ab, "~", 1);
+
+		if(y < E.screenrows -1) {
+			abAppend(ab, "\r\n", 2);
+		}
+	}
+}
+
+void editorRefreshScreen() {
+	struct abuf ab = ABUF_INIT;
+	
+	abAppend(&ab, "\x1b[2j", 4);
+	abAppend(&ab, "\x1b[H", 3);
+
+	editorDrawRows(&ab);
+
+	abAppend(&ab, "\x1b[H", 3);
+	
+	write(STDOUT_FILENO, ab.b, ab.len);
+	abFree(&ab);
+}
+
+int main() {
+	enableRawMode();
+	initEditor();
+
+	while(1) {
+		editorRefreshScreen();
+		editorProcessKeyPress();
+	}
+
+	return 0;
+}
