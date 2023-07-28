9404a551ee3fe2684b3a0f762b1d228fe28bb433
bl03360 <bkl050398@gmail.com>
2023-07-28 15:53:40 -0400

    Implemented message bar

--- kilo.c
+++ kilo.c
@@ -426,7 +426,7 @@ void initEditor() {
 		die("getWindowSize");
 	}
 
-	E.screenrows -= 1;
+	E.screenrows -= 2;
 }
 
 void editorScroll() {
@@ -525,6 +525,17 @@ void editorDrawStatusBar(struct abuf *ab) {
 	abAppend(ab, "\x1b[m", 3);
 }
 
+void editorDrawMessageBar(struct abuf *ab) {
+	abAppend(ab, "\x1b[K", 3);
+	int msglen = strlen(E.statusmsg);
+	if(msglen > E.screencols) {
+		msglen = E.screencols;
+	}
+	if(msglen && time(NULL) - E.statusmsg_time < 5) {
+		abAppend(ab, E.statusmsg, msglen);
+	}
+}
+
 void editorRefreshScreen() {
 	editorScroll();
 
@@ -535,6 +546,7 @@ void editorRefreshScreen() {
 
 	editorDrawRows(&ab);
 	editorDrawStatusBar(&ab);
+	editorDrawMessageBar(&ab);
 
 	char buf[32];
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
